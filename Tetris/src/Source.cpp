aec39fb7be8712032a29f321150c0237ebd62c68
Lucifer1662 <l.hawk99@hotmail.com>
2018-08-13 22:52:24 +1000

    Improved Version that doesn't such up heaps of cpu

--- Tetris/src/Source.cpp
+++ Tetris/src/Source.cpp
@@ -6,43 +6,41 @@
 #include <GLHelpers/Program.h>
 #include <GLHelpers/Buffer.h>
 #include <string>
-#include <time.h>
+#include <array>
+#include <algorithm>
+#include <chrono> 
+
 using std::string;
 
 typedef glm::tvec2<int, glm::precision::mediump> ivec2;
 using glm::vec2;
 const int gridWidth = 10;
 const int gridHeight = 20;
-
 const int numOfBockTypes=7;
-
 const float downSpeed = 0.1f;
 const float horizontalSpeed = 0.1f;
-
 const float speeds[3] = {horizontalSpeed, horizontalSpeed, downSpeed};
-
-
-
 const int blockSpeed = 30;
 const ivec2 gridSize = { gridWidth, gridHeight };
 const int blockSize = 30;
 ivec2 screenSize;
-
-float blockFallSpeed = 0.3f;
-
+const float blockFallSpeed = 0.3f;
 
 
+
+float randf() {
+	return rand() / (float)INT16_MAX;
+}
 
 struct Block {
 	static unsigned int blockProgram;
 	static Buffer squareBuffer;
 	static int offsetLocation;
 	static int colourLocation;
-	ivec2 pos;
-	glm::vec3 colour;
-	bool isDrawable = false;
+	static glm::vec3 colours[UINT8_MAX];
 
-	Block(ivec2 pos, glm::vec3 colour, bool isDrawable = false) : pos(pos), colour(colour), isDrawable(isDrawable) {}
+	unsigned char colourId;
+	Block(unsigned char colour = 0): colourId(colour) {}
 
 	static void Init() {
 		squareBuffer.CreateBuffer();
@@ -53,6 +51,9 @@ struct Block {
 				x,y, x,-y, -x,-y };
 			squareBuffer.SetData(verts, sizeof(verts));
 		}
+
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
 		string vert = R"V0G0N(
 		#version 430
@@ -79,119 +80,156 @@ struct Block {
 		glUseProgram(blockProgram);
 		offsetLocation = glGetUniformLocation(blockProgram, "offset");
 	 	colourLocation = glGetUniformLocation(blockProgram, "colour");
-		
-		
+
+		colours[0] = { 0,0,0 };
+		for (size_t i = 1; i < sizeof(colours)/sizeof(glm::vec3); i++)
+			colours[i] = { randf(), randf(), randf() };
 	}
 
-	void Render() {
-		if (isDrawable) {
-			glUniform3fv(colourLocation, 1, &colour[0]);
+	void Render(glm::vec2 pos) {
+		Block::Render(pos, colourId);
+	}
 
+	static void Render(glm::vec2 pos, unsigned char colour) {
+		if (colour > 0) {
 			//need to convert to screen space
-			glm::vec2 p = pos;
-			p.x = (p.x / (float)gridSize.x + 1 / (gridSize.x / 0.5f)) * 2 - 1;
-			p.y = (p.y / (float)gridSize.y + 1 / (gridSize.y / 0.5f)) * 2 - 1;
-			glUniform2fv(offsetLocation, 1, &p[0]);
+			pos.x = (pos.x / (float)gridSize.x + 1 / (gridSize.x / 0.5f)) * 2 - 1;
+			pos.y = (pos.y / (float)gridSize.y + 1 / (gridSize.y / 0.5f)) * 2 - 1;
+			glUniform2fv(offsetLocation, 1, &pos[0]);
+			glUniform3fv(colourLocation, 1, &colours[colour][0]);
 			squareBuffer.Bind();
 			glDrawArrays(GL_TRIANGLES, 0, 6);
 		}
 	}
+
+	bool isReal() const { return colourId != 0; }
 };
 
 unsigned int Block::blockProgram = 0;
 Buffer Block::squareBuffer = Buffer(GL_ARRAY_BUFFER, GL_STATIC_DRAW, false);
 int Block::offsetLocation;
 int Block::colourLocation;
+glm::vec3 Block::colours[UINT8_MAX];
 
-
-
-int at(ivec2 pos) {
-	return pos.x * gridHeight + pos.y;
-}
-
-bool isBlockHere(const std::vector<Block> blocks, ivec2 pos) {
-	for (auto b = blocks.begin(); b != blocks.end(); b++) {
-		if (b->pos == pos)
-			return true;
+struct Grid {
+	typedef std::vector<Block> Row;
+	std::array<Row, gridHeight> rows;
+	Grid(){
+		for (size_t i = 0; i < rows.size(); i++)
+			rows[i] = std::vector<Block>(gridWidth);
 	}
-	return false;
-}
+	bool isWithinGrid(const ivec2& pos) const {
+		return pos.x >= 0 && pos.y >= 0 && pos.x < gridWidth && pos.y < gridHeight;
+	}
 
-struct FallingPiece {
-	static const std::vector<std::vector<ivec2>> pieces[];
-	int rotation;
-	ivec2 pos;
-
-	std::vector<std::vector<Block>> blocks;
-	FallingPiece(int type, glm::vec3 colour): rotation(0) {
-		pos = ivec2(gridWidth / 2, gridHeight);
-		blocks.resize(pieces[type].size());
-		for (size_t i = 0; i < pieces[type].size(); i++)
+	bool isBlockHere(ivec2 pos) const {
+		if (!isWithinGrid(pos))
+			return true;
+		return rows[pos.y][pos.x].isReal();
+	}
+	void Add(ivec2 p, unsigned char colour) {
+		if(isWithinGrid(p))
+			rows[p.y][p.x].colourId = colour;
+	}
+	void Render() {
+		for (size_t y = 0; y < rows.size(); y++)
+			for (size_t x = 0; x < rows[y].size(); x++)
+				rows[y][x].Render(glm::vec2(x, y));
+	}
+	void removeSwap(std::vector<Block>& blocks, int i) {
+		blocks[i] = blocks.back();
+		blocks.pop_back();
+	}
+	bool isFull(const Row& row) {
+		return std::all_of(row.begin(), row.end(), [](const Block& block) {return block.isReal(); });
+	}
+	void ClearRow(Row& row) {
+		memset(row.data(), 0, row.size() * sizeof(Row::value_type));
+	}
+	void DoRemoval() {
+		for (size_t i = 0; i < rows.size(); i++)
 		{
-			blocks[i].reserve(pieces[type][i].size());
-			for (size_t j = 0; j < pieces[type][i].size(); j++)
-			{
-				blocks[i].emplace_back(pieces[type][i][j], colour, true);
+			if (isFull(rows[i])) {
+				std::copy(rows.begin() + i+1, rows.end(), rows.begin()+i);
+				ClearRow(rows.back());
+				i--;
 			}
 		}
 	}
-	void Move(ivec2 direction, const std::vector<Block>& constBlocks) {
-		if (CanMoveThisWay(direction, constBlocks))
+
+
+};
+
+
+
+
+struct FallingPiece {
+	typedef std::vector<ivec2> Piece;
+	typedef std::vector<Piece> RotationPiece;
+	static const RotationPiece pieces[];
+
+	int rotation;
+	ivec2 pos;
+	unsigned char colourId;
+	const RotationPiece* piece;
+
+	FallingPiece(int type, unsigned char colour = rand()% (UINT8_MAX-1) + 1)
+		: colourId(colour), rotation(0), piece(&pieces[type]) {
+		pos = ivec2(gridWidth / 2, gridHeight);
+	}
+
+	void Move(ivec2 direction, const Grid& grid) {
+		if (CanMoveThisWay(direction, grid))
 			//move all the blocks
 			pos += direction;
-		//for (auto b = blocks[rotation].begin(); b != blocks[rotation].end(); b++) {
-		//	b->pos += direction;
-		//}
 	}
 
 	void Render() {
-		for (auto b = blocks[rotation].begin(); b != blocks[rotation].end(); b++) {
-			b->pos += pos;
-			b->Render();
-			b->pos -= pos;
+		for (auto blockPos = CurrentPiece().begin(); blockPos != CurrentPiece().end(); blockPos++) {
+					Block::Render(*blockPos + pos, colourId);
 		}
 	}
 
-	bool ConflictingBlocks(ivec2 direction, const std::vector<Block>& blocks, const std::vector<Block>& constBlocks) {
-
+	bool ConflictingBlocks(ivec2 direction, const Grid& grid) {
 		ivec2 p;
-		for (auto b = blocks.begin(); b != blocks.end(); b++) {
-			p = b->pos + direction + pos;
-			if (p.x < 0 || p.x >= gridWidth || isBlockHere(constBlocks, p) || p.y == -1)
+		for (auto positions = CurrentPiece().begin();
+			positions != CurrentPiece().end(); positions++) {
+			p = *positions + direction + pos;
+			if (p.x < 0 || p.x >= gridWidth || grid.isBlockHere(p) && p.y<gridHeight || p.y == -1)
 				return true;
 		}
 		return false;
 	}
 
-	bool CanMoveThisWay(ivec2 direction, const std::vector<Block>& constBlocks) {
-		return !ConflictingBlocks(direction, blocks[rotation], constBlocks);
+	bool CanMoveThisWay(ivec2 direction, const Grid& grid) {
+		return !ConflictingBlocks(direction, grid);
 	}
 
-	void Rotate(const std::vector<Block> constBlocks) {
-		int rot = rotation + 1;
-		if (rot >= blocks.size())
-			rot = 0;
-		if (!ConflictingBlocks({ 0,0 }, blocks[rot], constBlocks))
+	void Rotate(const Grid& grid) {
+		int rot = rotation;
+		rotation++;
+		if (rotation >= piece->size())
+			rotation = 0;
+		if (ConflictingBlocks({ 0,0 }, grid))
 			rotation = rot;
-
 	}
 
-	bool hasLost() {
-		for (auto b = blocks[rotation].begin(); b < blocks[rotation].end(); b++)
+	bool hasLoss() {
+		for (auto b = CurrentPiece().begin(); b < CurrentPiece().end(); b++)
 		{
-			if (b->pos.y + pos.y >= gridHeight)
+			if (b->y + pos.y >= gridHeight)
 				return true;
 		}
 		return false;
 	}
-	
-	const std::vector<Block> GetBlocks() {
-		std::vector<Block> b = blocks[rotation];
-		for (size_t i = 0; i < b.size(); i++)
-		{
-			b[i].pos += pos;
-		}
-		return b;
+
+	const Piece& CurrentPiece() {
+		return (*piece)[rotation];
+	}
+
+	void AddToGrid(Grid& grid) {
+		for (auto p = CurrentPiece().begin(); p != CurrentPiece().end(); p++)
+			grid.Add(pos + *p, colourId);
 	}
 
 
@@ -234,57 +272,12 @@ const std::vector<std::vector<ivec2>> FallingPiece::pieces[numOfBockTypes]{
 	{ { 0,0 },{-1,0},{1,0},{-1,-1} },
 	{ { 0,0 },{0,1},{0,-1},{-1,1} },
 }
-
-
 };
 
 
-void removeSwap(std::vector<Block>& blocks, int i) {
-	blocks[i] = blocks.back();
-	blocks.pop_back();
-}
-
-void DoRemoval(std::vector<Block>& blocks) {
-	int count[gridHeight];
-	memset(count, 0, sizeof(count));
-	for (size_t i = 0; i < blocks.size(); i++)
-	{
-		count[blocks[i].pos.y] ++;
-	}
-
-	int removals[gridHeight];
-	memset(removals, 0, sizeof(removals));
-	int lowestLevel = gridHeight+1;
-	for (size_t i = 0; i < blocks.size(); i++)
-	{
-		int lvl = blocks[i].pos.y;
-		if (count[lvl] == gridWidth) {
-			if (lowestLevel < lvl)
-				lowestLevel = lvl;
-			removeSwap(blocks, i--);
-
-			removals[lvl] = 1;
-		}
-	}
-
-	for (size_t i = 1; i < gridHeight; i++)
-	{
-		removals[i] += removals[i-1];
-	}
-
-	for (size_t i = 0; i < blocks.size(); i++)
-	{
-		blocks[i].pos.y -= removals[blocks[i].pos.y];
-	}
-}
-
-float randf() {
-	return rand() / (float)INT16_MAX;
-}
 
 #include <Windows.h>
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
 //int main(int argc, char** argv) {
 	glfwInit();
 	screenSize = { blockSize * gridSize.x, blockSize * gridSize.y };
@@ -294,67 +287,48 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 
 	glewInit();
 	glClearColor(0.5f, 0.5f, 0.5f, 1);
-	
 	glfwSetWindowPos(window, 0, 40);
 
 	Block::Init();
 
-
-	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(
-		0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-		2,                  // size
-		GL_FLOAT,           // type
-		GL_FALSE,           // normalized?
-		0,                  // stride
-		(void*)0            // array buffer offset
-	);
-
 	srand(clock());
-	while (true)
-	{
 
-		std::vector<Block> blocks;
+	while (true){
 
-		FallingPiece piece = FallingPiece(rand() % numOfBockTypes, { randf(),randf(),randf() });
+		Grid grid = Grid();
 
+		FallingPiece piece = FallingPiece(rand() % numOfBockTypes);
 
 		float timeSinceLastMovedDown = 0;
 		float timeSinceLastPressed[4];
 		bool released[4]{ 1,1,1,1 };
-		clock_t start;
+		auto start = std::chrono::high_resolution_clock::now();
 		float delta = 0;
-
 		while (true) {
-			start = clock();
-			glfwPollEvents();
-			glfwSwapBuffers(window);
-			glClear(GL_COLOR_BUFFER_BIT);
+			start = std::chrono::high_resolution_clock::now();
+			glfwWaitEventsTimeout(blockFallSpeed/30.0f);
 
 
-			if (timeSinceLastMovedDown > blockFallSpeed) {
-				if (!piece.CanMoveThisWay({ 0,-1 }, blocks)) {
-					if (piece.hasLost())
+			if (blockFallSpeed < timeSinceLastMovedDown) {
+				if (!piece.CanMoveThisWay({ 0,-1 }, grid)) {
+					if (piece.hasLoss())
 						break;
-					std::vector<Block> b = piece.GetBlocks();
-					blocks.insert(blocks.end(), b.begin(), b.end());
-					piece = FallingPiece(rand() % numOfBockTypes, { randf(),randf(),randf() });
-					DoRemoval(blocks);
-					
+					piece.AddToGrid(grid);
+					piece = FallingPiece(rand() % numOfBockTypes);
+					grid.DoRemoval();
 				}
 
 				timeSinceLastMovedDown = 0;
-				piece.Move({ 0,-1 }, blocks);
+				piece.Move({ 0,-1 }, grid);
 			}
-
 			static ivec2 dir[3] = { {1,0},{-1,0},{0,-1} };
 
 			for (size_t i = 0; i < 3; i++)
 			{
 				if (glfwGetKey(window, GLFW_KEY_RIGHT+i) == GLFW_PRESS) {
-					if (released[i] == true)
-						piece.Move(dir[i], blocks);
+					if (released[i] == true) {
+						piece.Move(dir[i], grid); 
+					}
 					released[i] = false;
 					timeSinceLastPressed[i] += delta;
 				}
@@ -365,41 +339,33 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 				if (!released[i]) {
 					if (timeSinceLastPressed[i] > speeds[i]) {
 						timeSinceLastPressed[i] = 0;
-						piece.Move(dir[i], blocks);
+						piece.Move(dir[i], grid);
 					}
 				}
 			}
-		
+
 
 			if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
 				if (released[3] == true)
-					piece.Rotate(blocks);
+					piece.Rotate(grid); 
 				released[3] = false;
 			}
 
-			if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_RELEASE) {
+			if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_RELEASE)
 				released[3] = true;
-			}
 			
-			if (glfwWindowShouldClose(window)) {
+			if (glfwWindowShouldClose(window))		
 				return 0;
-			}
 	
-
 			//Render blocks
-
-			for (auto b = blocks.begin(); b != blocks.end(); b++)
-				b->Render();
-
+			grid.Render();
 			piece.Render();
-
-			delta = (clock() - start) / (float)CLOCKS_PER_SEC;
-			timeSinceLastMovedDown += delta;
+			glfwSwapBuffers(window);
+			glClear(GL_COLOR_BUFFER_BIT);
 			
+			delta = ((std::chrono::duration<double>)(std::chrono::high_resolution_clock::now() - start)).count();
+			timeSinceLastMovedDown += delta;
 		}
-
 	}
-
-
 	return 0;
 }
